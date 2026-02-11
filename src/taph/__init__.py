94cc7c1abd32e04ea51b5f2760e69a11e1fe39cc
mesotron.dev <mail@mesotron.dev>
2026-02-11 17:17:37 +0000

    patch: release v0.1.1
    FossilOrigin-Name: 811d04771eae2352467cda22bbe004514ade4ab25bbb5a8b1936615777e73e80

--- src/taph/__init__.py
+++ src/taph/__init__.py
@@ -3,7 +3,7 @@
 from taph.core import Immutable, Namespace, freeze, is_immutable
 from taph.exceptions import ImmutableError, TaphError
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __all__ = (
     'Namespace',
     'Immutable',
